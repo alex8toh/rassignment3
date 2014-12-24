8fdacea507d9bc7b28bb017fc3f98dd58e8c4216
Alex Toh <alex8toh@gmail.com>
2014-12-23 22:16:23 -0800

    implement best.R

--- best.R
+++ best.R
@@ -5,19 +5,27 @@ best <- function(state, outcome)
   
   ## Check that state and outcome are valid
   # get the distinct values for states 
-  states = unique(data[,8]) 
+  states = unique(data[,7]) 
   if (!is.element(state, states)) 
       stop("Invalid State!")
   
-  outcomes = c("heart attack", "heart failure", "pneumonia")
+  outcomes <- c("heart attack", "heart failure", "pneumonia")
+  
+  # outcomes column index
+  ocols <- c(11,17,13)
   if (!is.element(outcome, outcomes))
           stop("Invalid Outcome!")
-      
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
   
+  # the col index to the desired outcome 
+  ocol = ocols[match(outcome,outcomes)]
   
+  # get the outcomes that belongs to the state
+  sdata <- data[data$State==state, ]
   
+  # get the index to min  
+  idx <- which.min(as.double(sdata[, ocol]))
   
-  
+  ## Return hospital name (coloumn 2) in that state with 
+  ## lowest 30-day death rate
+  sdata[idx,2]
 }
